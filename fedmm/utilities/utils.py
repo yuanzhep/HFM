67222480278e01af9e1ba0936d4d87043c4717b3
yuanzhep <93398024+yuanzhep@users.noreply.github.com>
2024-01-14 15:53:36 -0500

    avg

---
+++ fedmm/utilities/utils.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@author: yz
+01/13/2024
+"""
+
+import numpy as np
+
+def average_models(model, client_models):
+    num_clients = len(client_models)
+    theta_sum = np.zeros(model.shape)
+    for theta in client_models:
+        theta_sum += theta
+    model = theta_sum / num_clients
+    return model
