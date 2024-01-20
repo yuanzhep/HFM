3aab0225e1cf14cdd56b2f4f1d682b9f1b07d33b
yuanzhep <93398024+yuanzhep@users.noreply.github.com>
2024-01-20 15:10:49 -0500

    Update fedmm_yz_0115.py

--- fedmm/fedmm_yz_0115.py
+++ fedmm/fedmm_yz_0115.py
@@ -32,7 +32,7 @@ from sklearn.preprocessing import MinMaxScaler
 from tqdm import tqdm
 from typing import Dict
 from typing import Any
-# from load_m40 import load_modelnet_10_data
+from load_m40 import load_modelnet_40_data
 from fedmm.utilities.utils import average_models
 import torch.nn as nn
 import torch.nn.functional as F
