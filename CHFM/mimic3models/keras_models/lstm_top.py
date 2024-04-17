7cbf4d007d423774e8c2b1268472dc24cadea746
yuanzhep <93398024+yuanzhep@users.noreply.github.com>
2024-04-17 19:29:23 -0400

    lstm

---
+++ CHFM/mimic3models/keras_models/lstm_top.py
@@ -0,0 +1,69 @@
+# 0410/2024, HFC
+
+from __future__ import absolute_import
+from __future__ import print_function
+
+import tensorflow as tf
+from keras.models import Model
+from keras.layers import Input, Dense, LSTM, Masking, Dropout
+from keras.layers.wrappers import Bidirectional, TimeDistributed
+from mimic3models.keras_utils import LastTimestep
+from mimic3models.keras_utils import ExtendMask
+
+
+class Network(Model):
+
+    def __init__(self, dim, batch_norm, dropout, rec_dropout, task,
+                 target_repl=False, deep_supervision=False, num_classes=1,
+                 depth=1, input_dim=76, multi=False, downstream_clients=1, **kwargs):
+
+        print("==> not used params in network class:", kwargs.keys())
+
+        self.dim = dim
+        self.batch_norm = batch_norm
+        self.dropout = dropout
+        self.rec_dropout = rec_dropout
+        self.depth = depth
+
+        if task in ['decomp', 'ihm', 'ph']:
+            final_activation = 'sigmoid'
+        elif task in ['los']:
+            if num_classes == 1:
+                final_activation = 'relu'
+            else:
+                final_activation = 'softmax'
+        else:
+            raise ValueError("Wrong value for task")
+
+        L = Input(shape=dim*downstream_clients, name='X') 
+
+        if dropout > 0:
+            L = Dropout(dropout)(L)
+
+        if target_repl:
+            y = TimeDistributed(Dense(num_classes, activation=final_activation),
+                                name='seq')(L)
+            y_last = LastTimestep(name='single')(y)
+            outputs = [y_last, y]
+        elif deep_supervision:
+            y = TimeDistributed(Dense(num_classes, activation=final_activation))(L)
+            y = ExtendMask()([y, M])  
+            outputs = [y]
+        else:
+            y = None
+            if multi:
+                y = Dense(num_classes)(L)
+            else:
+                y = Dense(num_classes, activation=final_activation)(L)
+            outputs = [y]
+
+        super(Network, self).__init__(inputs=L, outputs=outputs)
+
+    def say_name(self):
+        return "{}.n{}{}{}{}.dep{}".format('k_lstm',
+                                           self.dim,
+                                           ".bn" if self.batch_norm else "",
+                                           ".d{}".format(self.dropout) if self.dropout > 0 else "",
+                                           ".rd{}".format(self.rec_dropout) if self.rec_dropout > 0 else "",
+                                           self.depth)
+    
