2853d21bca9848bd0d5e01793ee38c4a2442f302
yuanzhep <93398024+yuanzhep@users.noreply.github.com>
2024-06-03 11:39:50 -0400

    read

---
+++ CHFM/mimic3benchmark/readers.py
@@ -0,0 +1,222 @@
+# 0527/2024, HFC, yz
+
+from __future__ import absolute_import
+from __future__ import print_function
+
+import os
+import numpy as np
+import random
+
+class Reader(object):
+    def __init__(self, dataset_dir, listfile=None):
+        self._dataset_dir = dataset_dir
+        self._current_index = 0
+        if listfile is None:
+            listfile_path = os.path.join(dataset_dir, "listfile.csv")
+        else:
+            listfile_path = listfile
+        with open(listfile_path, "r") as lfile:
+            self._data = lfile.readlines()
+        self._listfile_header = self._data[0]
+        self._data = self._data[1:]
+
+    def get_number_of_examples(self):
+        return len(self._data)
+
+    def random_shuffle(self, seed=None):
+        if seed is not None:
+            random.seed(seed)
+        random.shuffle(self._data)
+
+    def read_example(self, index):
+        raise NotImplementedError()
+
+    def read_next(self):
+        to_read_index = self._current_index
+        self._current_index += 1
+        if self._current_index == self.get_number_of_examples():
+            self._current_index = 0
+        return self.read_example(to_read_index)
+
+class DecompensationReader(Reader):
+    def __init__(self, dataset_dir, listfile=None):
+        Reader.__init__(self, dataset_dir, listfile)
+        self._data = [line.split(',') for line in self._data]
+        self._data = [(x, float(t), int(y)) for (x, t, y) in self._data]
+
+    def _read_timeseries(self, ts_filename, time_bound):
+        ret = []
+        with open(os.path.join(self._dataset_dir, ts_filename), "r") as tsfile:
+            header = tsfile.readline().strip().split(',')
+            assert header[0] == "Hours"
+            for line in tsfile:
+                mas = line.strip().split(',')
+                t = float(mas[0])
+                if t > time_bound + 1e-6:
+                    break
+                ret.append(np.array(mas))
+        return (np.stack(ret), header)
+
+    def read_example(self, index):
+        if index < 0 or index >= len(self._data):
+            raise ValueError("Index must be from 0 to number of examples.")
+        name = self._data[index][0]
+        t = self._data[index][1]
+        y = self._data[index][2]
+        (X, header) = self._read_timeseries(name, t)
+
+        return {"X": X,
+                "t": t,
+                "y": y,
+                "header": header,
+                "name": name}
+
+class InHospitalMortalityReader(Reader):
+    def __init__(self, dataset_dir, listfile=None, period_length=48.0):
+        Reader.__init__(self, dataset_dir, listfile)
+        self._data = [line.split(',') for line in self._data]
+        self._data = [(x, int(y)) for (x, y) in self._data]
+        self._period_length = period_length
+
+    def _read_timeseries(self, ts_filename):
+        ret = []
+        with open(os.path.join(self._dataset_dir, ts_filename), "r") as tsfile:
+            header = tsfile.readline().strip().split(',')
+            assert header[0] == "Hours"
+            for line in tsfile:
+                mas = line.strip().split(',')
+                ret.append(np.array(mas))
+        return (np.stack(ret), header)
+
+    def read_example(self, index):
+        if index < 0 or index >= len(self._data):
+            raise ValueError("Index must be from 0 to number of lines.")
+
+        name = self._data[index][0]
+        t = self._period_length
+        y = self._data[index][1]
+        (X, header) = self._read_timeseries(name)
+
+        return {"X": X,
+                "t": t,
+                "y": y,
+                "header": header,
+                "name": name}
+
+class LengthOfStayReader(Reader):
+    def __init__(self, dataset_dir, listfile=None):
+        Reader.__init__(self, dataset_dir, listfile)
+        self._data = [line.split(',') for line in self._data]
+        self._data = [(x, float(t), float(y)) for (x, t, y) in self._data]
+
+    def _read_timeseries(self, ts_filename, time_bound):
+        ret = []
+        with open(os.path.join(self._dataset_dir, ts_filename), "r") as tsfile:
+            header = tsfile.readline().strip().split(',')
+            assert header[0] == "Hours"
+            for line in tsfile:
+                mas = line.strip().split(',')
+                t = float(mas[0])
+                if t > time_bound + 1e-6:
+                    break
+                ret.append(np.array(mas))
+        return (np.stack(ret), header)
+
+    def read_example(self, index):
+        if index < 0 or index >= len(self._data):
+            raise ValueError("Index must be from 0 to number of lines.")
+
+        name = self._data[index][0]
+        t = self._data[index][1]
+        y = self._data[index][2]
+        (X, header) = self._read_timeseries(name, t)
+
+        return {"X": X,
+                "t": t,
+                "y": y,
+                "header": header,
+                "name": name}
+
+class PhenotypingReader(Reader):
+    def __init__(self, dataset_dir, listfile=None):
+        Reader.__init__(self, dataset_dir, listfile)
+        self._data = [line.split(',') for line in self._data]
+        self._data = [(mas[0], float(mas[1]), list(map(int, mas[2:]))) for mas in self._data]
+
+    def _read_timeseries(self, ts_filename):
+        ret = []
+        with open(os.path.join(self._dataset_dir, ts_filename), "r") as tsfile:
+            header = tsfile.readline().strip().split(',')
+            assert header[0] == "Hours"
+            for line in tsfile:
+                mas = line.strip().split(',')
+                ret.append(np.array(mas))
+        return (np.stack(ret), header)
+
+    def read_example(self, index):
+        if index < 0 or index >= len(self._data):
+            raise ValueError("Index must be from 0 to number of lines.")
+
+        name = self._data[index][0]
+        t = self._data[index][1]
+        y = self._data[index][2]
+        (X, header) = self._read_timeseries(name)
+
+        return {"X": X,
+                "t": t,
+                "y": y,
+                "header": header,
+                "name": name}
+
+class MultitaskReader(Reader):
+    def __init__(self, dataset_dir, listfile=None):
+        Reader.__init__(self, dataset_dir, listfile)
+        self._data = [line.split(',') for line in self._data]
+
+        def process_ihm(x):
+            return list(map(int, x.split(';')))
+
+        def process_los(x):
+            x = x.split(';')
+            if x[0] == '':
+                return ([], [])
+            return (list(map(int, x[:len(x)//2])), list(map(float, x[len(x)//2:])))
+
+        def process_ph(x):
+            return list(map(int, x.split(';')))
+
+        def process_decomp(x):
+            x = x.split(';')
+            if x[0] == '':
+                return ([], [])
+            return (list(map(int, x[:len(x)//2])), list(map(int, x[len(x)//2:])))
+
+        self._data = [(fname, float(t), process_ihm(ihm), process_los(los),
+                       process_ph(pheno), process_decomp(decomp))
+                      for fname, t, ihm, los, pheno, decomp in self._data]
+
+    def _read_timeseries(self, ts_filename):
+        ret = []
+        with open(os.path.join(self._dataset_dir, ts_filename), "r") as tsfile:
+            header = tsfile.readline().strip().split(',')
+            assert header[0] == "Hours"
+            for line in tsfile:
+                mas = line.strip().split(',')
+                ret.append(np.array(mas))
+        return (np.stack(ret), header)
+
+    def read_example(self, index):
+        if index < 0 or index >= len(self._data):
+            raise ValueError("Index must be from 0 to number of lines.")
+
+        name = self._data[index][0]
+        (X, header) = self._read_timeseries(name)
+
+        return {"X": X,
+                "t": self._data[index][1],
+                "ihm": self._data[index][2],
+                "los": self._data[index][3],
+                "pheno": self._data[index][4],
+                "decomp": self._data[index][5],
+                "header": header,
+                "name": name}
